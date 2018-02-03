7b5f61a36b04daf667e8c6dc799793d69d2e61a9
lechiffre <le.chiffreua1@gmail.com>
2018-02-04 00:15:03 +0200

    #fix #chore (app.js) add / path into router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const webhookRouter = require('./routes/webhook.js')
 const app = express()
 
 app.use(bodyParser.json())
+app.get('/', (req, res) => { res.end('ok') })
 app.use('/webhook', webhookRouter)
 
 module.exports = app
